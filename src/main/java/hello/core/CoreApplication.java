172b7891fc646b6035becff03702ffe405f36538
Hoony <xognsdl710@naver.com>
2023-03-31 22:24:43 +0900

    2023.03.31 <2. 스프링 핵심 원리 - 기본편> - 정리

--- src/main/java/hello/core/CoreApplication.java
+++ src/main/java/hello/core/CoreApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CoreApplication {
-	// Spring web 현재 사용X
 
 	public static void main(String[] args) {
 		SpringApplication.run(CoreApplication.class, args);

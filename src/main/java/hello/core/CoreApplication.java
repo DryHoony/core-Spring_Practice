f46c28df53dc1d60548df93b0a0edc95b247b941
Hoony <xognsdl710@naver.com>
2023-04-13 18:09:29 +0900

    2023.04.13 <3. 모든 개발자를 위한 HTTP 웹 기본 지식> - 수강

--- src/main/java/hello/core/CoreApplication.java
+++ src/main/java/hello/core/CoreApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CoreApplication {
 	// 2. 스프링 핵심 원리 - 기본편
+	// 3. HTTP 웹 기본지식 - 강의수강
 
 	public static void main(String[] args) {
 		SpringApplication.run(CoreApplication.class, args);

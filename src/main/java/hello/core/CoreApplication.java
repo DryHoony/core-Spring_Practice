62ce18d6cd751b193d4aeb3757b4534c5022d614
Hoony <xognsdl710@naver.com>
2023-04-14 12:46:13 +0900

    2023.04.14 <4. 스프링 MVC 1편 - 백엔드 웹 개발 핵심 기술> - 수강중

--- src/main/java/hello/core/CoreApplication.java
+++ src/main/java/hello/core/CoreApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CoreApplication {
 	// 2. 스프링 핵심 원리 - 기본편
 	// 3. HTTP 웹 기본지식 - 강의수강
+	// 4. 스프링 MVC 1편 백엔드 웹 개발 핵심 기술 수강중
 
 	public static void main(String[] args) {
 		SpringApplication.run(CoreApplication.class, args);
